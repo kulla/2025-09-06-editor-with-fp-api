71f8deb53dd66a01587eaa071fb3e3f0abc561c1
Stephan Kulla <git.mail@kulla.me>
2025-09-06 18:57:52 +0200

    feat: Add debug panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { DebugPanel } from './components/debug-panel'
 
 export default function App() {
   return (
     <main className="prose p-10">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <DebugPanel
+        labels={{ example: 'Example' }}
+        getCurrentValue={{ example: () => 'Hello, world!' }}
+        showOnStartup={{ example: true }}
+      />
     </main>
   )
 }
