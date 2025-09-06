6461274363d0e1203a1df7cfe88b0420cbf21c07
Stephan Kulla <git.mail@kulla.me>
2025-09-06 19:12:02 +0200

    feat(types): Add type "Entry"

--- src/types.ts
+++ src/types.ts
@@ -4,16 +4,27 @@ interface NodeMap {
   root: {
     jsonValue: { type: 'document'; document: JSONValue<'text'> }
     entryValue: Key<'text'>
+    parentKey: null
   }
   text: {
     jsonValue: string
     entryValue: Text
+    parentKey: Key<'root'>
   }
 }
 
 export type NodeType = keyof NodeMap
 
-export type Key<T extends NodeType> = `${T}:${number}`
+export type Key<T extends NodeType = NodeType> = `${T}:${number}`
 
 export type JSONValue<T extends NodeType> = NodeMap[T]['jsonValue']
 export type EntryValue<T extends NodeType> = NodeMap[T]['entryValue']
+export type ParentKey<T extends NodeType> = NodeMap[T]['parentKey']
+
+export type Entry<T extends NodeType> = { [S in T]: EntryOfType<S> }[T]
+interface EntryOfType<T extends NodeType> {
+  type: T
+  key: Key<T>
+  parentKey: ParentKey<T>
+  value: EntryValue<T>
+}
